820b8c9132beed61480d35fd59133c4cd4a69e3d
Krypton17 <maxdodon25@gmail.com>
2020-01-22 13:07:18 +0000

    factory for converting socket message to chat message

--- room-service/src/main/java/com/krypton/roomservice/service/factory/ChatMessageFactory.java
+++ room-service/src/main/java/com/krypton/roomservice/service/factory/ChatMessageFactory.java
@@ -1,4 +1,23 @@
 package com.krypton.roomservice.service.factory;
 
-public class ChatMessageFactory {
+import com.krypton.common.model.message.ChatMessage;
+import com.krypton.roomservice.feign.UserFeignClient;
+import com.krypton.roomservice.model.SocketChatMessageBody;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class ChatMessageFactory
+{
+  private final UserFeignClient userFeignClient;
+
+  public Optional<ChatMessage> buildFromSocketMessage(SocketChatMessageBody messageBody)
+  {
+    var user = userFeignClient.findById(messageBody.getAuthorId());
+
+    return user.map(value -> new ChatMessage(messageBody.getText(), value));
+  }
 }
