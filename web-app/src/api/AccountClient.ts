bf7b3625a67bf72574809b9dcf9272aba47878c0
Krypton17 <maxdodon25@gmail.com>
2020-01-22 13:48:03 +0000

    user account api client

---
+++ web-app/src/api/AccountClient.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+import { AuthReponse } from "../model/AuthResponse";
+import { User } from "../model/User";
+import { Room } from "../model/Room";
+
+const URL = "http://localhost:8080/account";
+
+export class AccountClient
+{
+  static login = (email : string, password : string) : Promise<AuthReponse> =>
+  (
+    axios.get(`${URL}/login?email=${email}&password=${password}`)
+      .then(response => response.data)
+      .catch(console.log)
+  );
+
+  static register = (nick : string, email : string, password : string) : Promise<AuthReponse> =>
+  (
+    axios.post(`${URL}/register?nick=${nick}&email=${email}&password=${password}`)
+      .then(response => response.data)
+      .catch(console.log)
+  );
+
+  static getFriends = (id : string) : Promise<User[]> =>
+  (
+    axios.get(`${URL}/get/friends?id=${id}`)
+      .then(response => response.data)
+      .catch(console.log)
+  );
+
+  static getRooms = (id : string) : Promise<Room[]> => 
+  (
+    axios.get(`${URL}/get/rooms?id=${id}`)
+      .then(response => response.data)
+      .catch(console.log)
+  );
+}
