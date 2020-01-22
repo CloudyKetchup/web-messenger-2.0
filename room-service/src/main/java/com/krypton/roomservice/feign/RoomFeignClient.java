a14d824daf50cc2847abb551bef8ec9b16744882
Krypton17 <maxdodon25@gmail.com>
2020-01-22 13:11:16 +0000

    rest logic base for room service

--- room-service/src/main/java/com/krypton/roomservice/feign/RoomFeignClient.java
+++ room-service/src/main/java/com/krypton/roomservice/feign/RoomFeignClient.java
@@ -1,6 +1,7 @@
 package com.krypton.roomservice.feign;
 
 import com.krypton.common.model.room.Room;
+import com.krypton.common.model.message.ChatMessage;
 import com.krypton.roomservice.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
@@ -10,17 +11,20 @@ import java.util.Set;
 import java.util.UUID;
 
 @FeignClient(name = "DATABASE-SERVICE", path = "/room", configuration = FeignConfig.class)
-public interface RoomFiegnClient
+public interface RoomFeignClient
 {
     @GetMapping("/get")
     Optional<Room> find(@RequestParam UUID id);
 
     @GetMapping("/get/all")
-    Set<Room> getAllRooms()
+    Set<Room> getAllRooms(@RequestBody UUID id);
 
     @PostMapping("/save")
     Room save(@RequestBody Room room);
 
     @DeleteMapping("/delete")
     void delete(@RequestBody UUID id);
+
+    @PutMapping("/message/add")
+    Optional<ChatMessage> addMessage(@RequestBody ChatMessage message, @RequestParam String room);
 }
--- room-service/src/main/java/com/krypton/roomservice/feign/UserFeignClient.java
+++ room-service/src/main/java/com/krypton/roomservice/feign/UserFeignClient.java
@@ -1,4 +1,16 @@
 package com.krypton.roomservice.feign;
 
-public class UserFeignClient {
+import com.krypton.common.model.user.User;
+import com.krypton.roomservice.config.FeignConfig;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@FeignClient(value = "database-service", url = "http://localhost:8200/user", configuration = FeignConfig.class)
+public interface UserFeignClient
+{
+  @GetMapping(value = "/get", params = "id")
+  Optional<User> findById(@RequestParam String id);
 }
