fafcd861d3a8a90da376c5cbea352cfd0bdca29b
Krypton17 <maxdodon25@gmail.com>
2020-01-29 11:23:37 +0200

    deleted notification service

--- notification-service/src/main/java/com/krypton/notificationservice/controller/MainController.java
+++
@@ -1,8 +0,0 @@
-package com.krypton.notificationservice.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/notification")
-public abstract class MainController {}
--- notification-service/src/main/java/com/krypton/notificationservice/service/INotificationService.java
+++
@@ -1,6 +0,0 @@
-package com.krypton.notificationservice.service;
-
-public interface INotificationService<N, ID>
-{
-  void sendNotification(N notification);
-}
