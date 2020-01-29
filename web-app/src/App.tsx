623e7b264fa94126c6835699f205e5c17825ec00
Krypton17 <maxdodon25@gmail.com>
2020-01-29 12:09:08 +0200

    friend requests pane on right side

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -3,27 +3,46 @@ import React, { Component } from 'react';
 import LeftPanelComponent from "./components/LeftPanel/LeftPanelComponent";
 import RoomComponent 			from "./components/RoomComponent/RoomComponent";
 import SearchComponent 		from "./components/SearchComponent/SearchComponent";
+import FriendRequestsPaneComponent from "./components/FriendRequestsPane/FriendRequestsPaneComponent";
 
 import { ProfileContextHelpers as Profile } 	from "./helpers/ProfileContextHelpers";
 import { AppContextHelpers as AppContext }  	from "./helpers/AppContextHelpers";
 import { RoomContextHelpers as RoomContext } 	from "./helpers/RoomContextHelpers";
 
 import { MessagingClient } from './api/MessagingClient';
+import { AccountSocketClient } from "./api/AccountSocketClient";
+
+import { Switch, Route, match } from 'react-router';
 
 import './index.css';
 
-export default class App extends Component
+type IProps = {
+	match : match
+};
+
+type IState = {
+	friendRequestsPane : boolean
+};
+
+export default class App extends Component<IProps, IState>
 {
+	state : IState = {
+		friendRequestsPane : false
+	};
 
 	componentDidMount = () =>
 	{
 		AppContext.createContext({ roomSelected : null }, this);
+
 		MessagingClient.init();
+		AccountSocketClient.init();
 	};
+
+	toggleFriendRequestsPane = () => this.setState({ friendRequestsPane : !this.state.friendRequestsPane });
 
 	render = () => (
 		<div className="main-container">
-			<LeftPanelComponent users={Profile.profileContext?.friends || []}/>
+			<LeftPanelComponent friends={Profile.profileContext?.friends || []} match={this.props.match}/>
 			{
 				RoomContext.context
 				&&
@@ -31,7 +50,25 @@ export default class App extends Component
 				&&
 				<RoomComponent data={RoomContext.context.data}/>
 			}
-			<SearchComponent/>
+			<Switch>
+				<Route path={`${this.props.match.url}/search`} component={SearchComponent}/>
+			</Switch>
+			{
+				!this.state.friendRequestsPane
+				?
+				<button
+					id="friend-requests-pane-toggle"
+					style={{ color : "white" }}
+					onClick={this.toggleFriendRequestsPane}
+				>
+					<i className="fas fa-user-plus"/>
+				</button>
+				:
+				<FriendRequestsPaneComponent
+					requests={Profile.profileContext?.friendRequests || []}
+					close={this.toggleFriendRequestsPane}
+				/>
+			}
 		</div>
 	);
 }
