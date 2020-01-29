30402acee685fd015652c6fbdf6ce053f25107e0
Krypton17 <maxdodon25@gmail.com>
2020-01-29 12:19:27 +0200

    registered component to AppContext

--- web-app/src/components/LeftPanel/LeftPanelComponent.tsx
+++ web-app/src/components/LeftPanel/LeftPanelComponent.tsx
@@ -1,24 +1,40 @@
 import React, { Component } from "react";
 
+import { Link, match } from "react-router-dom";
+
 import ListUserComponent from "../ListUserComponent/ListUserComponent";
 
 import { User } from "../../model/User";
 
 import "./list-panel-component.css";
+import { AppContextHelpers } from "../../helpers/AppContextHelpers";
 
-type IProps = { users: User[] };
+type IProps = {
+	friends : User[]
+	match : match
+};
+
+type IState = { friends : User[] };
 
 export default class LeftPanelComponent extends Component<IProps> {
+	state : IState = {
+		friends : this.props.friends
+	};
+
+	componentDidMount = () =>
+	{
+		AppContextHelpers.registerComponent(this);
+	};
 
 	render = () => (
 		<div className="left-panel">
 			<div className="people-pane">
-				{this.props.users.map(user => <ListUserComponent key={user.id} data={user} />)}
+				{this.state.friends.map(user => <ListUserComponent key={user.id} data={user} />)}
 			</div>
 			<div className="left-panel-control-buttons">
 				<div>
 					<button><i className="fas fa-plus" /></button>
-					<button><i className="fas fa-search" /></button>
+					<Link to={`${this.props.match.url}/search`}><i className="fas fa-search" /></Link>
 				</div>
 			</div>
 		</div>
