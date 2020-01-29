949a233da81a8e09c4e8abb27d34edfe2bbaaea5
Krypton17 <maxdodon25@gmail.com>
2020-01-29 12:24:01 +0200

    socket client for account-service

---
+++ web-app/src/api/FriendRequestClient.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+import { User } from "../model/User";
+
+const URL = "http://localhost:8080/account/request";
+
+export class FriendRequestClient
+{
+  static sendRequest = (from : string, to : string) : Promise<String> =>
+  (
+    axios.post(`${URL}/friendship/send?from=${from}&to=${to}`)
+      .then(response => response.data)
+      .catch(console.log)
+  );
+
+  static requestIsSent = (from : string, to : string) : Promise<Boolean> =>
+  (
+    axios.get(`${URL}/friendship/sent?from=${from}&to=${to}`)
+      .then(response => response.data)
+      .catch(console.log)
+  );
+
+  static acceptRequest = (requestId : string) : Promise<User> =>
+  (
+    axios.post(`${URL}/friendship/accept?id=${requestId}`)
+      .then(response => response.data)
+      .catch(console.log)
+  );
+
+  static deleteRequest = (id : string) => axios.delete(`${URL}/delete/friend-request?id=${id}`).catch(console.log);
+}
