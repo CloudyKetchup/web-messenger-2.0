6bfdbb2f1bfc04f57c2e9e11613f89aa9aa22c7a
Krypton17 <maxdodon25@gmail.com>
2020-02-01 01:30:24 +0200

    added new mappings

--- account-service/src/main/java/com/krypton/accountservice/controller/UserController.java
+++ account-service/src/main/java/com/krypton/accountservice/controller/UserController.java
@@ -3,6 +3,8 @@ package com.krypton.accountservice.controller;
 import com.krypton.accountservice.feign.UserFeignClient;
 import com.krypton.accountservice.model.SearchResultUser;
 import com.krypton.accountservice.service.UserService;
+import com.krypton.common.model.request.FriendRequest;
+import com.krypton.common.model.room.Room;
 import com.krypton.common.model.user.User;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
@@ -19,6 +22,24 @@ public class UserController extends MainController
 {
   private final UserFeignClient userFeignClient;
   private final UserService userService;
+
+  @GetMapping("/get/friends")
+  public Set<User> getFriends(@RequestParam String id)
+  {
+    return userService.getAllFriendsAsUsers(id);
+  }
+
+  @GetMapping("/get/friend-requests")
+  public Set<FriendRequest> getFriendRequests(@RequestParam String id)
+  {
+    return userService.getFriendRequests(id);
+  }
+
+  @GetMapping("/get/room")
+  public Optional<Room> getRoomByFriend(@RequestParam String userId, @RequestParam String friendId)
+  {
+    return userService.getRoomByFriend(UUID.fromString(userId), UUID.fromString(friendId));
+  }
 
   @GetMapping(value = "/search", params = "query")
   public Set<User> search(@RequestParam String query)
