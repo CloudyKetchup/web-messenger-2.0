e4e0d862a1155a03d2cf0d288e43f3b67e2b4b7b
Krypton17 <maxdodon25@gmail.com>
2020-02-01 23:20:16 +0200

    get messages mapping

--- room-service/src/main/java/com/krypton/roomservice/controller/RoomController.java
+++ room-service/src/main/java/com/krypton/roomservice/controller/RoomController.java
@@ -1,4 +1,25 @@
 package com.krypton.roomservice.controller;
 
-public class RoomController {
+import com.krypton.common.model.message.ChatMessage;
+import com.krypton.roomservice.feign.RoomFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RequestMapping("/room")
+@RestController
+@AllArgsConstructor
+public class RoomController
+{
+  private final RoomFeignClient roomFeignClient;
+
+  @GetMapping("/get/messages")
+  public Set<ChatMessage> getMessages(@RequestParam String id)
+  {
+    return roomFeignClient.getMessages(id);
+  }
 }
