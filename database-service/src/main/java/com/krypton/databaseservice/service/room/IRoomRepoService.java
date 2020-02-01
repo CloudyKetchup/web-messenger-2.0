f29eb913c8fa987d95398ab7de7a5af93d6eb328
Krypton17 <maxdodon25@gmail.com>
2020-02-01 23:19:14 +0200

    implemented getMessages and getMessagesAsStream

--- database-service/src/main/java/com/krypton/databaseservice/service/room/IRoomRepoService.java
+++ database-service/src/main/java/com/krypton/databaseservice/service/room/IRoomRepoService.java
@@ -4,12 +4,17 @@ import com.krypton.common.model.room.Room;
 import com.krypton.common.model.message.ChatMessage;
 import com.krypton.databaseservice.service.IModelService;
 
-import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 public interface IRoomRepoService extends IModelService<Room, UUID>
 {
   ChatMessage addMessage(Room room, ChatMessage message);
+
+  Set<ChatMessage> getMessages(UUID id);
+
+  Stream<ChatMessage> getMessagesAsStream(UUID id);
 
   void deleteMessage(Room room, ChatMessage message);
 }
