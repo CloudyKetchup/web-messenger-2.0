02597622dca53bce55f72967dc75c5a75fc0d9f6
Krypton17 <maxdodon25@gmail.com>
2020-02-07 10:59:54 +0200

    service for updating user model

--- account-service/src/main/java/com/krypton/accountservice/service/UserUpdater.java
+++ account-service/src/main/java/com/krypton/accountservice/service/UserUpdater.java
@@ -1,4 +1,34 @@
 package com.krypton.accountservice.service;
 
-public class UserUpdater {
+import com.krypton.accountservice.feign.UserFeignClient;
+import com.krypton.common.model.user.UserStatus;
+import lombok.AllArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class UserUpdater
+{
+  private final UserFeignClient userFeignClient;
+  private final SimpMessagingTemplate simpMessagingTemplate;
+
+  public void setStatus(UUID id, UserStatus status)
+  {
+    var user = userFeignClient.find(id);
+
+    user.ifPresent(u ->
+    {
+      userFeignClient.setStatus(id.toString(), status);
+
+      userFeignClient.find(id).ifPresent(updated -> sendUpdateToAllFriends(id.toString(), updated));
+    });
+  }
+
+  private void sendUpdateToAllFriends(String id, Object object)
+  {
+    simpMessagingTemplate.convertAndSend("/acc/user/" + id + "/friend/updated", object);
+  }
 }
--- database-service/src/main/java/com/krypton/databaseservice/service/user/UserUpdater.java
+++ database-service/src/main/java/com/krypton/databaseservice/service/user/UserUpdater.java
@@ -1,4 +1,27 @@
 package com.krypton.databaseservice.service.user;
 
-public class UserUpdater {
+import com.krypton.common.model.user.UserStatus;
+import com.krypton.databaseservice.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class UserUpdater
+{
+  private final UserRepository userRepository;
+
+  public void setStatus(UUID id, UserStatus status)
+  {
+    var user = userRepository.findById(id);
+
+    user.ifPresent(u ->
+    {
+      u.setStatus(status);
+
+      userRepository.save(u);
+    });
+  }
 }
