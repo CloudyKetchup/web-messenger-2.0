b600c50c1924c9e81a564a08c3af51dd95395e08
Krypton17 <maxdodon25@gmail.com>
2020-02-10 08:40:37 +0200

    basic image model/repo logic implementation

--- database-service/src/main/java/com/krypton/databaseservice/service/image/IImageService.java
+++ database-service/src/main/java/com/krypton/databaseservice/service/image/IImageService.java
@@ -1,4 +1,8 @@
 package com.krypton.databaseservice.service.image;
 
-public interface IImageService {
-}
+import com.krypton.common.model.media.Image;
+import com.krypton.databaseservice.service.IModelService;
+
+import java.util.UUID;
+
+public interface IImageService extends IModelService<Image, UUID> {}
