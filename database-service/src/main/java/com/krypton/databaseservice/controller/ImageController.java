a5c6b6cda7c65e37bb4acce6d9d2ccd098547f7e
Krypton17 <maxdodon25@gmail.com>
2020-02-10 08:39:57 +0200

    controller for image model

--- database-service/src/main/java/com/krypton/databaseservice/controller/ImageController.java
+++ database-service/src/main/java/com/krypton/databaseservice/controller/ImageController.java
@@ -1,4 +1,18 @@
 package com.krypton.databaseservice.controller;
 
-public class ImageController {
+import com.krypton.common.model.media.Image;
+import com.krypton.databaseservice.service.IModelService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/image")
+public class ImageController extends EntityController<Image, UUID>
+{
+  public ImageController(IModelService<Image, UUID> service)
+  {
+    super(service);
+  }
 }
