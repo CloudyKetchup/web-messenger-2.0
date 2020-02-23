7534635bd45fa12b772ac44be2f15f5e79347648
Krypton17 <maxdodon25@gmail.com>
2020-02-23 11:43:35 +0200

    separated socket init logic from componentDidMount method

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,30 +1,28 @@
 import React, { Component } from 'react';
+import { Switch, Route, match } from 'react-router';
 
 import LeftPanelComponent from "./components/LeftPanel/LeftPanelComponent";
 import RoomComponent 			from "./components/RoomComponent/RoomComponent";
 import SearchComponent 		from "./components/SearchComponent/SearchComponent";
 import UserInfoComponent	from "./components/UserInfoComponent/UserInfoComponent";
+import { NotificationPad }from "./components/Notification/NotificationPad/NotificationPad"
 import FriendRequestsPaneComponent from "./components/FriendRequestsPane/FriendRequestsPaneComponent";
-import { NotificationPad } from "./components/Notification/NotificationPad/NotificationPad"
 
 import { ProfileContextHelpers as Profile } 	from "./helpers/ProfileContextHelpers";
-import { AppContextHelpers as AppContext }  	from "./helpers/AppContextHelpers";
 import { RoomContextHelpers as RoomContext } 	from "./helpers/RoomContextHelpers";
+
+import * as PageEvents from "./util/events/PageEvents";
 
 import { MessagingClient } from './api/MessagingClient';
 import { AccountSocketClient } from "./api/AccountSocketClient";
 
-import { Switch, Route, match } from 'react-router';
-
 import { History } from "history";
-
-import * as PageEvents from "./util/events/PageEvents";
 
 import './index.css';
 
 type IProps = {
-	match : match
-	history : History
+	match    : match
+	history  : History
 };
 
 type IState = {
@@ -43,23 +41,26 @@ export default class App extends Component<IProps, IState>
 		{
 			this.props.history.push("/login");
 		}
-		AppContext.createContext({ roomSelected : null }, this);
-
-		MessagingClient.getInstance().init();
-		AccountSocketClient.getInstance().init();
+		this.initSockets();
 
 		PageEvents.initExitListener();
 	};
+
+  initSockets = () =>
+  {
+    MessagingClient.getInstance().init();
+		AccountSocketClient.getInstance().init();
+  };
 
 	toggleFriendRequestsPane = () => this.setState({ friendRequestsPane : !this.state.friendRequestsPane });
 
 	render = () => (
 		<div className="main-container">
-			<LeftPanelComponent friends={Profile.profileContext?.friends || []} match={this.props.match}/>
+			<LeftPanelComponent friends={Profile.profileContext?.friends || []}/>
 			<RoomComponent room={RoomContext.context} />
 			<Switch>
 				<Route path={`${this.props.match.url}/search`} component={SearchComponent}/>
-				<Route path={`${this.props.match.url}/user/info`} render={props => <UserInfoComponent {...props}/>}/>
+				<Route path={`${this.props.match.url}/user/info`} component={UserInfoComponent}/>
 			</Switch>
 			{
 				!this.state.friendRequestsPane
