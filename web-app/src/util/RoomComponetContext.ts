f7d294c9f23e513ddb268dbc90c88b8ac3afba37
Krypton17 <maxdodon25@gmail.com>
2020-02-23 11:49:37 +0200

    removed updateMessages

--- web-app/src/util/RoomComponetContext.ts
+++ web-app/src/util/RoomComponetContext.ts
@@ -2,15 +2,11 @@ import ComponentContext from "./ComponentContext";
 
 import RoomContext from "../context/RoomContext";
 
-import { Message } from "../model/Message";
-
 import RoomComponent from "../components/RoomComponent/RoomComponent";
-import RoomChatComponent from "../components/RoomChatComponent/RoomChatComponent";
 
 export class RoomComponentContext extends ComponentContext
 {
   private static instance : RoomComponentContext;
-  private static roomChatComponent : RoomChatComponent;
   private static roomComponent : RoomComponent;
 
   static getInstance = () : RoomComponentContext =>
@@ -29,17 +25,5 @@ export class RoomComponentContext extends ComponentContext
       RoomComponentContext.roomComponent = this.components.filter(c => c instanceof RoomComponent)[0] as RoomComponent;
     }
     RoomComponentContext.roomComponent && RoomComponentContext.roomComponent.setState({ room : room });
-  };
-
-  updateMessages = (messages : Message[]) =>
-  {
-    if (!RoomComponentContext.roomChatComponent)
-    {
-      RoomComponentContext.roomChatComponent = this.components.filter(c => c instanceof RoomChatComponent)[0] as RoomChatComponent;
-    }
-    if (RoomComponentContext.roomChatComponent)
-    {
-      RoomComponentContext.roomChatComponent.setState({ messages : messages }, RoomComponentContext.roomChatComponent.scrollBottom)
-    }
   };
 }
